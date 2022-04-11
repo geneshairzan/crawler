07ce1be1427072ba2beec89b5484fff08d83c472
geneshairzan <genesha.irzan@gmail.com>
2022-04-11 13:03:50 +0700

    gip

--- app/lib/crawler.js
+++ app/lib/crawler.js
@@ -39,7 +39,8 @@ export function dataparsing(body, config) {
 export async function getHTML(config) {
   console.log(`Start Crawling ${config.url}`);
   const browser = await puppeteer.launch({
-    headless: config.headless || true,
+    executablePath: "/usr/bin/firefox",
+    headless: true,
   });
   const page = await browser.newPage();
   await page.setViewport({ width: 1280, height: 800 });
