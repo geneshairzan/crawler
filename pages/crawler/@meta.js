ca6040aa09b25a8c1dee9442c9a0bc08447b6999
geneshairzan <genesha.irzan@gmail.com>
2022-05-13 19:58:26 +0700

    gip

--- pages/crawler/@meta.js
+++ pages/crawler/@meta.js
@@ -2,7 +2,7 @@ export const meta = {
   model: "crawler",
   col: [
     { name: "name", label: "Names", w: "20%" },
-    { name: "url", w: "20%" },
+    { name: "url", w: "80%" },
     // { name: "material", w: "20%" },
     // { name: "finishing", w: "20%" },
     // { name: "unitprice", w: "5%" },
